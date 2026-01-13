5c4a7c047078206d7a5eee58ddcb635bf6538fe0
Jean-Baptiste Vaugirard <jea.vau@dwwm.afpa>
2026-01-13 16:45:42 +0100

    ptit maj code et maj

--- src/Entity/Coupon.php
+++ src/Entity/Coupon.php
@@ -14,7 +14,7 @@ class Coupon
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 10)]
+    #[ORM\Column(length: 10, unique: true)]
     private ?string $code = null;
 
     #[ORM\Column(type: Types::TEXT)]
